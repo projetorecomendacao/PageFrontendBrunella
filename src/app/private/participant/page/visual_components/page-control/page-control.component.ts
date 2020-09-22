1979dabdf3d3ee52bb0034b8e7ebff241c60046c
augustodantas <laurentino.dantas@usp.br>
2020-09-22 01:18:27 -0300

    versao 22092020

---
+++ src/app/private/participant/page/visual_components/page-control/page-control.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-control',
+  templateUrl: './page-control.component.html'
+})
+export class PageControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
