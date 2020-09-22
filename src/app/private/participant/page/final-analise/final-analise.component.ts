07b94d132a147b34af62d2559197006e45d9c15e
augustodantas <laurentino.dantas@usp.br>
2020-09-22 10:40:49 -0300

    22092020_gerar_PDF

--- src/app/private/participant/page/final-analise/final-analise.component.ts
+++ src/app/private/participant/page/final-analise/final-analise.component.ts
@@ -3,6 +3,9 @@ import { FormGroup } from '@angular/forms';
 import { ChecaCampo } from 'src/app/shared/checa-campo';
 import { Router } from '@angular/router';
 import { PageService } from '../page.service';
+import * as jspdf from 'jspdf';  
+import html2canvas from 'html2canvas'; 
+
 
 @Component({
   selector: 'app-final-analise',
@@ -12,11 +15,15 @@ export class FinalAnaliseComponent implements OnInit {
 
   @Input() pageForm: FormGroup;
 
+  nome : string;
+
   conta:number=0;
   
   constructor(private checaCampo: ChecaCampo, private router: Router, private pageService : PageService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.nome = this.pageService.participant.getName();
+  }
 
   // método que verifica a situação dos campos do form
   mudou(campo: string): string{ 
@@ -42,7 +49,22 @@ export class FinalAnaliseComponent implements OnInit {
     this.pageService.submit(this.pageForm);    
     alert('PAGe salvo com sucesso!!');
     this.router.navigate(['private/']).then();
+  }
 
+  gerarPDF(){
+    var _nome = document.getElementById('div_pdf');  
+    html2canvas(_nome).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 180;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      var position = 0;  
+      let pdf = new jspdf.jsPDF('p', 'mm', 'a4');
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
 
   }
 }
